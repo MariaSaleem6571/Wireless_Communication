407b967891e1086b2a112af02d1df8a1f73f5b88
Maria <maria.saleem1999@gmail.com>
2023-11-30 19:32:13 +0100

    "Added Synchornization"

--- trasmitter/trasmitter.ino
+++ trasmitter/trasmitter.ino
@@ -1,28 +1,33 @@
 #define LED_PIN 6
 #define DATA_SIZE 8
-#define BIT_0_DELAY 1000
-#define BIT_1_DELAY 250
+#define BIT_0_DELAY 2000
+#define BIT_1_DELAY 1000
 #define CLOCK_DELAY 100
-#define SYNC_PATTERN 0b01111110 // Example sync pattern
+#define SYNC_DELAY 500
+// #define SYNC_PATTERN 0b00000000 // Example sync pattern
 
 
 // Store binary data as byte type
-byte data[] = {0, 1, 1, 0, 1, 1, 1, 0};
-//byte data[] = {0, 0, 0, 0, 0, 0, 0, 0};
+byte sync[] = {0, 0, 0, 0, 0, 0, 0, 0};
+byte data[] = {0, 0, 0, 0, 0, 0, 0, 0};
 bool lastStateHigh = false; 
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
+  delay(1000);
+  sendStartPulse();
+  fmmod();
 }
 
 void sendStartPulse() {
+  // Keep track of previous state
+  Serial.println("Start sending starting pulse");
   lastStateHigh = !lastStateHigh;
   digitalWrite(LED_PIN, lastStateHigh ? HIGH : LOW);
-  delay(5000); // Starting pulse
-}
-
- // Keep track of previous state
-
+  delay(3000); // Starting pulse
+  Serial.println("Ending starting pulse");
+} 
 
 void transmitBit(byte bitVal) {
   
@@ -31,6 +36,7 @@ void transmitBit(byte bitVal) {
   //delay(CLOCK_DELAY); // Short delay for clock pulse
   
   if (bitVal == 0) {
+    lastStateHigh = !lastStateHigh; // Toggle the last state for the next bit
     // For '0', for the whole bit period
     digitalWrite(LED_PIN, lastStateHigh ? HIGH : LOW);
     // Delay for the entire bit period
@@ -38,33 +44,42 @@ void transmitBit(byte bitVal) {
   } else {
     // For '1', change mid-way through the bit period
     // First half
+    lastStateHigh = !lastStateHigh; // Toggle the last state for the next bit
     digitalWrite(LED_PIN, lastStateHigh ? LOW : HIGH);  
     delay(BIT_1_DELAY);
     // Second half 
+    lastStateHigh = !lastStateHigh; // Toggle the last state for the next bit
     digitalWrite(LED_PIN, lastStateHigh ? HIGH : LOW); 
     delay(BIT_1_DELAY);
-    lastStateHigh = !lastStateHigh; // Toggle the last state for the next bit
   }
 }
 
+void syncbit(byte syncval) {
 
-
+  if (syncval == 0) {
+    // For '0'
+    digitalWrite(LED_PIN, LOW);
+    delay(SYNC_DELAY); 
+  } else {
+    // For '1'
+    digitalWrite(LED_PIN, HIGH);  
+    delay(SYNC_DELAY);
+  }
+}
 void fmmod() {
-  
-  // Transmit sync pattern first
-  for (int i = 7; i >= 0; i--) { 
-    // Transmit each bit of the sync pattern
-    transmitBit((SYNC_PATTERN >> i) & 1);
-  }
-
-  // Transmit actual data
+    // Transmit actual data
   for (int i = 0; i < DATA_SIZE; i++) {
-    transmitBit(data[i]);
+    trasmitbit(sync[i]);
+    Serial.println("sync");
   }
+  // Transmit actual data
+   for (int i = 0; i < DATA_SIZE; i++) {
+     transmitBit(data[i]);
+     Serial.println("data");
+   }
+  
 }
 
 void loop() {
-  sendStartPulse();
-  fmmod();
   delay(5000); // Delay before repeating the sequence
 }
